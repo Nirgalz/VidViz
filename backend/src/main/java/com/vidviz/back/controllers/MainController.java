03495c4921fb3bac188f8840dc3ca52bb1ed8812
NH <znirgal@gmail.com>
2020-03-19 11:56:52 +0100

    first commit

---
+++ backend/src/main/java/com/vidviz/back/controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.vidviz.back.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    // Forwards all routes to FrontEnd except: '/', '/index.html', '/api', '/api/**'
+    // Required because of 'mode: history' usage in frontend routing, see README for further details
+    @RequestMapping(value = "{_:^(?!index\\.html|api).$}")
+    public String redirectApi() {
+        //LOG.info("URL entered directly into the Browser, so we need to redirect...");
+        return "forward:/";
+    }
+}

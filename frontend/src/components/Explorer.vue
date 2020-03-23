2c67eb83e62cfa0c4cb2769177b35a6683327a0e
NH <znirgal@gmail.com>
2020-03-23 16:02:54 +0100

    play multiple videos

--- frontend/src/components/Explorer.vue
+++ frontend/src/components/Explorer.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <b-row class="text-center">
-            <b-col>
+            <b-col cols="2">
                 <b-table  hover :items="folders" @row-clicked="(item, index, event) => loadFiles(item)"></b-table>
             </b-col>
-            <b-col cols="8">
+            <b-col>
+                <b-btn variant="success" @click="playPauseVideos"> > </b-btn>
                 <div v-for="item in files" :key="item.name">
                     <video-player
                             ref="videoPlayer"
-                            :options="{sources:[{src:item.url}]}"
+                            :options="{
+                        sources:[{src:item.url}],
+                        controls:false
+                    }"
                     >
                     </video-player>
                 </div>
@@ -27,9 +31,18 @@ import UploadService from "../services/UploadFilesService";
                 folders: [],
                 files: [],
                 selectedFolder: "",
+                players: []
             }
         },
         methods: {
+            playPauseVideos() {
+                this.players = this.$refs.videoPlayer;
+                console.log(this.players);
+                for ( let i = 0 ; i < this.players.length ; i++) {
+                    console.log(this.players[i].player);
+                    this.players[i].player.play();
+                }
+            },
             selectFolder(item) {
                 this.selectedFolder = item.name;
             },

9fc44da6c39dbf0cfd302c78bfddf8b88554f6d2
NH <znirgal@gmail.com>
2020-03-26 17:08:34 +0100

    fixes

--- backend/src/main/java/com/vidviz/back/controller/FilesController.java
+++ backend/src/main/java/com/vidviz/back/controller/FilesController.java
@@ -17,20 +17,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 
 
-@Controller
-@CrossOrigin("http://localhost:8081")
+@RestController
+//@CrossOrigin("http://localhost:8081")
 public class FilesController {
 
     private static final Logger LOG = LoggerFactory.getLogger(FilesController.class);
